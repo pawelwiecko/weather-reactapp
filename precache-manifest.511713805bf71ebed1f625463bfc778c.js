a4e727ecf5982b2c408914132a7340bfd327cb89
Pawel Wiecko <pawelwiecko2018@gmail.com>
2020-04-30 10:23:36 +0200

    Updates

---
+++ precache-manifest.511713805bf71ebed1f625463bfc778c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a78d3c51e38c00293c051c2420241b2",
+    "url": "/weather-reactapp/static/media/weather.5a78d3c5.jpg"
+  },
+  {
+    "revision": "15c71d8b994244199fb8",
+    "url": "/weather-reactapp/static/js/runtime~main.78c2285f.js"
+  },
+  {
+    "revision": "4747c5dd56af7382ce24",
+    "url": "/weather-reactapp/static/js/main.642bd09b.chunk.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/weather-reactapp/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "4747c5dd56af7382ce24",
+    "url": "/weather-reactapp/static/css/main.d21b5f80.chunk.css"
+  },
+  {
+    "revision": "6624f95fc898440e7c3d3bd39d358712",
+    "url": "/weather-reactapp/index.html"
+  }
+];
